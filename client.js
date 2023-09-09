56809b47e11fa32bc330053642b45fc39ea3feee
Davi <davicabral01@gmail.com>
2023-09-09 10:19:31 -0300

    fim da calculadora

--- client.js
+++ client.js
@@ -1,10 +1,9 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const grpc = require("@grpc/grpc-js");
-const loader = require('@grpc/proto-loader');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 
-const packageDefinitionAdd = loader.loadSync('./add-config/add.proto', {
+const PROTO_PATH = './calc-config/calculator.proto';
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
   longs: String,
   enums: String,
@@ -12,107 +11,40 @@ const packageDefinitionAdd = loader.loadSync('./add-config/add.proto', {
   oneofs: true,
 });
 
-const packageDefinitionSubtract = loader.loadSync('./subtract-config/subtract.proto', {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
+const calculatorProto = grpc.loadPackageDefinition(packageDefinition).Calculator;
 
-const packageDefinitionMultiply = loader.loadSync('./multiply-config/multiply.proto', {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
+const client = new calculatorProto('localhost:60055', grpc.credentials.createInsecure());
 
-const packageDefinitionDivide = loader.loadSync('./divide-config/divide.proto', {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
-// Carregar mais packageDefinition para multiplicação e divisão
+const operands = { operand1: 158, operand2: 2 };
 
-const addProto = grpc.loadPackageDefinition(packageDefinitionAdd);
-const subtractProto = grpc.loadPackageDefinition(packageDefinitionSubtract);
-const multiplyProto = grpc.loadPackageDefinition(packageDefinitionMultiply);
-const dividetProto = grpc.loadPackageDefinition(packageDefinitionDivide);
-
-// Carregar mais protos para multiplicação e divisão
-
-const addClient = new addProto.Add('localhost:60051', grpc.credentials.createInsecure());
-const subtractClient = new subtractProto.Subtract('localhost:60052', grpc.credentials.createInsecure());
-const multiplyClient = new multiplyProto.Multiply('localhost:60053', grpc.credentials.createInsecure());
-const divideClient = new dividetProto.Divide('localhost:60054', grpc.credentials.createInsecure());
-
-// Criar mais clientes para multiplicação e divisão
-
-const operands = { operand1: 10, operand2: 5 };
-
-addClient.Add(operands, (error, response) => {
+client.Add(operands, (error, response) => {
   if (!error) {
-    console.log('Soma:', response.value);
+    console.log('Resultado da Adição:', response.value);
   } else {
-    console.error('Error:', error);
+    console.error('Erro na Adição:', error);
   }
 });
 
-subtractClient.Subtract(operands, (error, response) => {
+client.Subtract(operands, (error, response) => {
   if (!error) {
-    console.log('Subtração:', response.value);
+    console.log('Resultado da Subtração:', response.value);
   } else {
-    console.error('Error:', error);
+    console.error('Erro na Subtração:', error);
   }
 });
 
-multiplyClient.Multiply(operands, (error, response) => {
+client.Multiply(operands, (error, response) => {
   if (!error) {
-    console.log('Multiplicação:', response.value);
+    console.log('Resultado da Multiplicação:', response.value);
   } else {
-    console.error('Error:', error);
+    console.error('Erro na Multiplicação:', error);
   }
 });
 
-divideClient.Divide(operands, (error, response) => {
+client.Divide(operands, (error, response) => {
   if (!error) {
-    console.log('Divisão:', response.value);
+    console.log('Resultado da Divisão:', response.value);
   } else {
-    console.error('Error:', error);
+    console.error('Erro na Divisão:', error);
   }
-});
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    const filePath = path.join(__dirname, 'frontend', 'index.html');
-    const contentType = 'text/html';
-    
-    fs.readFile(filePath, 'utf8', (err, content) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Error loading the file');
-      } else {
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(content);
-      }
-    });
-  }
-});
-let addBut = document.getElementById('add');
-addBut.addEventListener(('click'), ()=>{
-  addClient.Add(operands, (error, response) => {
-    if (!error) {
-      console.log('Soma:', response.value);
-    } else {
-      console.error('Error:', error);
-    }
-  });
-})
-// Capturar eventos dos botões e enviar solicitações gRPC...
-
-server.listen(8080, () => {
-  console.log('Server running at http://localhost:8080/');
 });
